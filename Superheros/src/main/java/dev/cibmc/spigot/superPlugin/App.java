fcd91ea2531f9da47a46249068f23ba05de18c5a
Devin Vering <devin.vering@gmail.com>
2022-12-19 20:31:14 -0600

    Initialized more maven stuff

---
+++ Superheros/src/main/java/dev/cibmc/spigot/superPlugin/App.java
@@ -0,0 +1,13 @@
+package dev.cibmc.spigot.superPlugin;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class App extends JavaPlugin {
+    @Override
+    public void onEnable() {
+        getLogger().info("Hello, SpimingotMC!");
+    }
+    @Override
+    public void onDisable() {
+        getLogger().info("See you again, SpigotMC!");
+    }
+}
