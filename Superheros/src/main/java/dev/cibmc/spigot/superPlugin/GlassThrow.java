fcfb9a70cb83a7cbae9e1b0325d243b050ea0b18
Devin Vering <devin.vering@gmail.com>
2023-01-01 21:46:27 -0600

    Added block iterator to sonic boom

--- Superheros/src/main/java/dev/cibmc/spigot/superPlugin/GlassThrow.java
+++ Superheros/src/main/java/dev/cibmc/spigot/superPlugin/GlassThrow.java
@@ -1,66 +1,86 @@
 package dev.cibmc.spigot.superPlugin;
 
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 
-import org.bukkit.Bukkit;
 import org.bukkit.entity.*;
-import org.bukkit.Effect;
+import org.bukkit.Bukkit;
 import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.Particle;
-import org.bukkit.Server;
 import org.bukkit.Sound;
-import org.bukkit.Vibration;
 import org.bukkit.block.Block;
-import org.bukkit.block.data.type.NoteBlock;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.Action;
 import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.potion.Potion;
+import org.bukkit.potion.PotionEffect;
+import org.bukkit.potion.PotionEffectType;
+import org.bukkit.util.BlockIterator;
+import org.bukkit.util.RayTraceResult;
 
 public class GlassThrow implements Listener {
-    
     @EventHandler
-    public static void onRightClick(PlayerInteractEvent event) throws InterruptedException {
+    public void addSpeed(PlayerJoinEvent event) {
         Player player = event.getPlayer();
-        Block block = player.getTargetBlock(null, 200);
-
-        if ((event.getAction() == Action.PHYSICAL) && (player.getDisplayName().equals("Devster41"))) {
-            player.setWalkSpeed((float) 0.3);
+        if (player.getDisplayName().equals("Devster41")) {
+            player.addPotionEffect(new PotionEffect(PotionEffectType.SPEED, Integer.MAX_VALUE, 2));
         }
+    }
 
-        if (event.getAction() == Action.LEFT_CLICK_AIR) {
-            if (player.getDisplayName().equals("Devster41")) {
+    @EventHandler
+    public static void onClick(PlayerInteractEvent event) throws InterruptedException {
+        Player player = event.getPlayer();
+
+        //Amanda
+        if (player.getDisplayName().equals("Devster41")) {
+            if (event.getAction() == Action.LEFT_CLICK_AIR) {
                 ItemStack i = new ItemStack(Material.NOTE_BLOCK);
-                if (event.getItem().getItemMeta().equals(i.getItemMeta()) && (Math.abs(player.getLocation().distanceSquared(block.getLocation())) > 20)) {
-                    double midy = (block.getY() + player.getLocation().getY()) / 2;
-                    double midx = (block.getX() + player.getLocation().getX()) / 2;
-                    double midz = (block.getZ() + player.getLocation().getZ()) / 2;
-                    double midmid = (player.getLocation().distanceSquared(block.getLocation()) / 4);
 
-                    Location mid = new Location(player.getWorld(), midx, midy, midz);
+                if (event.getItem().getItemMeta().equals(i.getItemMeta())) {
+                    BlockIterator iterator = new BlockIterator(player, 10);
+                    Block nextBlock = null;
+                    Collection<Entity> blockNearby = null;
+                    while (iterator.hasNext()) {
+                        nextBlock = iterator.next();
+                        blockNearby = nextBlock.getLocation().getWorld().getNearbyEntities(nextBlock.getLocation(), 2, 2, 2);
+                        player.spawnParticle(Particle.SONIC_BOOM, nextBlock.getLocation(), 10);
+                        for (Entity tmp : blockNearby) {
+                            if ((tmp instanceof Damageable) && !(tmp instanceof Player))
+                            ((Damageable) tmp).damage(3);
+                        }
+                    }
 
-                    Location tf = new Location(player.getWorld(), midx + midmid, 
-                                                midy + midmid, 
-                                                midz + midmid);
+                     
+                    /* 
+                    Collection<Entity> blockNearby = block.getLocation().getWorld().getNearbyEntities(block.getLocation(), 2, 2, 2);
 
-                    Location of = new Location(player.getWorld(), midx + midmid, 
-                                                midy + midmid, 
-                                                midz + midmid);
-
-                    Collection<Entity> blockNearby = block.getLocation().getWorld().getNearbyEntities(block.getLocation(), 5, 5, 5);
-
-                    player.spawnParticle(Particle.SONIC_BOOM, of, 50);
-                    Thread.sleep(100);
-                    player.spawnParticle(Particle.SONIC_BOOM, mid, 50);
-                    Thread.sleep(100);
-                    player.spawnParticle(Particle.SONIC_BOOM, tf, 50);
-                    Thread.sleep(100);
-                    player.spawnParticle(Particle.SONIC_BOOM, block.getLocation(), 50);
+                    
+                    List<Entity> enemies = new ArrayList<>();
                     for (Entity tmp: blockNearby) {
                         if ((tmp instanceof Damageable) && !(tmp instanceof Player))
+                            enemies.add(tmp);
+                    }
+
+                   
+                    double midy = (block.getLocation().getY() + player.getLocation().getY()) / 2;
+                    double midx = (block.getLocation().getX() + player.getLocation().getX()) / 2;
+                    double midz = (block.getLocation().getZ() + player.getLocation().getZ()) / 2;
+
+                    Location mid = new Location(player.getWorld(), midx, midy, midz);
+                    Thread.sleep(100);
+                    player.spawnParticle(Particle.SONIC_BOOM, mid, 1);
+                    Thread.sleep(100); 
+                    */
+                   
+                    
+                    for (Entity tmp: blockNearby) {
+                        if ((tmp instanceof Damageable) && !(tmp instanceof Player))
+                            player.spawnParticle(Particle.SONIC_BOOM, tmp.getLocation(), 30);
                             ((Damageable) tmp).damage(3);
                     }
                 }
